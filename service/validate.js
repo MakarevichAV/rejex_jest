6e50ccf0143b8a5ada1603727edbb16c7c3d194f
MakarevichAV <dunkanm89@gmail.com>
2026-02-19 21:07:13 +0200

    HW

--- service/validate.js
+++ service/validate.js
@@ -15,4 +15,38 @@ const validateEmail = text => {
     return pattern.test(text)
 }
 
-export {validateECMAScriptText, validateDomainZone, validateEmail}
+
+//HW
+
+// Validate credit cards (8-16 numbers)
+const validateCreditCard = text => {
+    const pattern = /^[0-9]{8,16}$/
+    return pattern.test(text)
+}
+
+//  Validate date DD.MM.YYYY
+const validateDate1 = text => {
+    const pattern = /^([0-2][0-9]|3[0-1])\.(0[0-9]|1[0-2])\.[0-9]{4}$/
+    return pattern.test(text)
+}
+
+//  Validate date YYYY-MM-DD
+const validateDate2 = text => {
+    const pattern = /^[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$/
+    return pattern.test(text)
+}
+
+//Phone Number (Format: +99(99)9999-9999)
+const validatePhoneNumber = text => {
+    const pattern = /^\+[0-9]{2}\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/
+    return pattern.test(text)
+}
+
+//Number less and equals 255 (positive)
+const validateOctet = text => {
+    const pattern = /^(\d{1,2}|[0-1][0-9][0-9]|2[0-5]{2})$/
+    return pattern.test(text)
+}
+
+export {validateECMAScriptText, validateDomainZone, validateEmail, validateCreditCard,
+        validateDate1, validateDate2, validatePhoneNumber, validateOctet}
--- service/validate.test.js
+++ service/validate.test.js
@@ -1,4 +1,5 @@
-import {validateECMAScriptText, validateDomainZone, validateEmail} from './validate'
+import {validateECMAScriptText, validateDomainZone, validateEmail, validateOctet,
+        validateCreditCard, validateDate1, validateDate2, validatePhoneNumber} from './validate'
 import {describe, test, expect} from '@jest/globals'
 
 describe('validateECMAScriptText', () => {
@@ -54,7 +55,79 @@ describe('validateEmail', () => {
 
 
 
+//HW
 
+// Validate credit cards (8-16 numbers)
+describe('validateCreditCard', () => {
+    test('Card number should be valid', () => {
+        expect(validateCreditCard('12543678')).toBeTruthy()
+        expect(validateCreditCard('6606667778')).toBeTruthy()
+        expect(validateCreditCard('1234567891011127')).toBeTruthy()
+    })
+    test('Card number should not be valid', () => {
+        expect(validateCreditCard('1234567')).toBeFalsy()
+        expect(validateCreditCard('123456789123456789')).toBeFalsy()
+        expect(validateCreditCard('12345a6789')).toBeFalsy()
+    })
+})
+
+//  Validate date DD.MM.YYYY
+describe('validateDate1', () => {
+    test('Date should be valid', () => {
+        expect(validateDate1('01.01.1990')).toBeTruthy()
+        expect(validateDate1('31.12.2026')).toBeTruthy()
+    })
+    test('Date should not be valid', () => {
+        expect(validateDate1('32.09.1999')).toBeFalsy()
+        expect(validateDate1('01.13.2000')).toBeFalsy()
+        expect(validateDate1('02.e3.2222')).toBeFalsy()
+        expect(validateDate1('02/11/2222')).toBeFalsy()
+        expect(validateDate1('02-04-2222')).toBeFalsy()
+    })
+})
+
+//  Validate date YYYY-MM-DD
+describe('validateDate2', () => {
+    test('Date should be valid', () => {
+        expect(validateDate2('1990-01-01')).toBeTruthy()
+        expect(validateDate2('2026-12-31')).toBeTruthy()
+    })
+    test('Date should not be valid', () => {
+        expect(validateDate2('1999-09-32')).toBeFalsy()
+        expect(validateDate2('2000-13-01')).toBeFalsy()
+        expect(validateDate2('2222-e3-02')).toBeFalsy()
+        expect(validateDate2('2222/11/02')).toBeFalsy()
+        expect(validateDate2('2222.04.02')).toBeFalsy()
+    })
+})
+
+//Phone Number (Format: +99(99)9999-9999)
+describe('validatePhoneNumber', () => {
+    test('Phone number should be valid', () => {
+        expect(validatePhoneNumber('+99(99)9999-9999')).toBeTruthy()
+    })
+    test('Phone number should not be valid', () => {
+        expect(validatePhoneNumber('+99(99)9999_9999')).toBeFalsy()
+        expect(validatePhoneNumber('99(99)9999-9999')).toBeFalsy()
+        expect(validatePhoneNumber('+99999999-9999')).toBeFalsy()
+        expect(validatePhoneNumber('+99(99)999f-9999')).toBeFalsy()
+        expect(validatePhoneNumber('+99(99)99999999')).toBeFalsy()
+    })
+})
+
+//Number less and equals 255 (positive)
+describe('validateOctet', () => {
+    test('Octet should be valid', () => {
+        expect(validateOctet('192')).toBeTruthy()
+        expect(validateOctet('0')).toBeTruthy()
+        expect(validateOctet('255')).toBeTruthy()
+    })
+    test('Octet should not be valid', () => {
+        expect(validateOctet('-1')).toBeFalsy()
+        expect(validateOctet('256')).toBeFalsy()
+        expect(validateOctet('ff')).toBeFalsy()
+    })
+})
 
 
 
